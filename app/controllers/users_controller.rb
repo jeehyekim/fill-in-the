ed6354088f842d84d4b040e434c029dd0b61850a
jeehyekim <iam@jeehyekim.com>
2015-11-18 09:57:44 -0800

    created signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+
+  private
+  def user_params
+    user_params = params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+  end
+
+end
