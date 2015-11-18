9ff7b99619cfbb61e96a7d25bdc4ae712677ac09
noel-arce <n.arcellana.00@gmail.com>
2015-11-18 14:46:22 -0800

    created quiz new/show

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,24 @@
 class QuizzesController < ApplicationController
+
+	def new
+		@quiz = Quiz.new
+		render :new
+	end
+
+	def create
+		@quiz = Quiz.create(quiz_params)
+			redirect_to quiz_path(@quiz)
+		# render :show
+	end
+
+	def show
+		@quiz = Quiz.find(params[:id])
+		render :show
+	end
+
+	private
+		def quiz_params
+			quiz_params = params.require(:quiz).permit(:title, :content)
+		end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: [:new, :create, :show]
+  resources :quizzes, only: [:new, :create, :show]
 
 end
