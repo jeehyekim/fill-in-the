939d99413ba69c8ad31a2ce14d8c7cc7105b1484
noel-arce <n.arcellana.00@gmail.com>
2015-11-18 14:05:06 -0800

    user and quiz initial model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118020523) do
+ActiveRecord::Schema.define(version: 20151118194403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    # t.text     "keyword",    default: [],              array: true
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
