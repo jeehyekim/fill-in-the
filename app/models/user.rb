1fcd35a38b9add3db68a86351479dc5840322ae6
jeehyekim <iam@jeehyekim.com>
2015-11-18 10:09:39 -0800

    validation edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, length: {minimum: 6}, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
-  validates :password, presence: true, length: {minimum: 8}
+  validates :password, presence: true, length: {minimum: 3}
   validates :username, presence: true
 
 end
