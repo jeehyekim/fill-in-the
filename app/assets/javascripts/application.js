c5875e7d4c253dcd525baeda7bdb3ff6de9f2e45
jvbarisone@gmail.com <YOUR-EMAIL-ADDRESS>
2015-11-18 17:25:15 -0800

    Word replace functionality working(partially)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,36 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var body = "This is a sentence shorp. We will take words from it.";
+
+var keywords = ["sentence","words"];
+
+// Compares body array 
+function keyCompare (paragraph, key) {
+    var answerKey = [];
+    for (var i=0; i < key.length; i++){
+        for (var j=0; j <= paragraph.length; j++){
+            if (paragraph[j] == key[i]){
+                answerKey.push([paragraph.indexOf(key[i]), paragraph[j]]);
+            }
+        }
+    }
+    return answerKey;
+}
+
+var answerKey = keyCompare(body.split(' '), keywords);
+
+function numberReplace(original, answerKey){
+    for (var j = 0; j < answerKey.length; j++){
+        original[answerKey[j][0]] = j + 1;
+        }
+    return original.join(' ');
+    }
+    
+numberReplace(body.split(' '), answerKey);
+
+
+
+
+
