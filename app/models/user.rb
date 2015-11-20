2237c68b8997f2d63211945f1be567e3380c7260
jvbarisone@gmail.com <YOUR-EMAIL-ADDRESS>
2015-11-20 14:05:46 -0800

    Paperclip default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, length: {minimum: 6}, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
   validates :username, presence: true
 
-  has_attached_file :image, styles: { thumb: "150x150#" }
-  # , :default_url => "/images/defaultpic.png"
+  has_attached_file :image, styles: { thumb: "150x150#" }, :default_url => "missing.png"
+ 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
