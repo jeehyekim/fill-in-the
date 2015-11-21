d85d26d6deb4837785624f0b86a225c3c51b3b90
jeehyekim <iam@jeehyekim.com>
2015-11-21 14:30:22 -0800

    added edit and delete links on quiz show; other minor styling changes; created quiz edit page

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,6 @@ class Quiz < ActiveRecord::Base
 	# serialize :keywords, Array
 	belongs_to :users
 
-
 	validates :content, presence: true
 	validates :title, presence: true
 	validates :keyword, presence: true
