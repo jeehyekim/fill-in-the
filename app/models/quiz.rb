8f70f066101f75c6031be208474ea6245171b04c
jvbarisone@gmail.com <YOUR-EMAIL-ADDRESS>
2015-11-22 17:25:56 -0800

    Enrichment model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,6 @@
 class Quiz < ActiveRecord::Base
 	belongs_to :user
 	has_many :enrichments, dependent: :destroy
-	has_many :users, through: :enrichments
 
 	validates :content, presence: true
 	validates :title, presence: true
